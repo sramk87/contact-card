4c72bc2340f2d0b4b2ae164392f80cf83a054627
sramk87 <sramkissoon87@gmail.com>
2022-10-24 12:48:42 -0400

    indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,7 +12,10 @@ import Dog from '../images/dog.png';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-window.addEventListener('load', function () {
+import { initdb } from './database';
+
+window.addEventListener('load', function() {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
